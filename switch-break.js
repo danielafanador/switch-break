d552d84901596eec39b3212a17f28528246d76af
Daniel Afanador <danielthemisfits@gmail.com>
2021-10-14 12:35:38 -0300

    Agregamos una opción que devuelve un mensaje en caso de que haya jugadas inválidas

--- switch-break.js
+++ switch-break.js
@@ -5,24 +5,28 @@ var jugador02 = prompt("Elige piedra, papel o tijera: ");
 
 if (jugador01 === jugador02) {console.log("Es un empate!")}
 
-    else {
+    else if (jugador01 != "piedra" || "papel" || "tijera") {console.log ("Jugaste un valor inválido");}
+    else if (jugador02 != "piedra" || "papel" || "tijera") {console.log ("Jugaste un valor inválido");}
 
-        switch (jugador01) {
-            case "piedra":
-                if (jugador02 === "papel") {console.log("Gana el jugador 02");}
-                    else if (jugador02 === "tijera") {console.log("Gana el jugador 01");}
+
+        else {
+
+            switch (jugador01) {
+                case "piedra":
+                    if (jugador02 === "papel") {console.log("Gana el jugador 02");}
+                        else if (jugador02 === "tijera") {console.log("Gana el jugador 01");}
+            }
+
+            switch (jugador01) {
+                case "papel":
+                    if (jugador02 === "tijera") {console.log("Gana el jugador 02");}
+                        else if (jugador02 === "piedra") {console.log("Gana el jugador 01");}
+            }
+
+            switch (jugador01) {
+                case "tijera":
+                    if (jugador02 === "piedra") {console.log("Gana el jugador 02");}
+                        else if (jugador02 === "papel") {console.log("Gana el jugador 01");}
+            }
+
         }
-
-        switch (jugador01) {
-            case "papel":
-                if (jugador02 === "tijera") {console.log("Gana el jugador 02");}
-                    else if (jugador02 === "piedra") {console.log("Gana el jugador 01");}
-        }
-
-        switch (jugador01) {
-            case "tijera":
-                if (jugador02 === "piedra") {console.log("Gana el jugador 02");}
-                    else if (jugador02 === "papel") {console.log("Gana el jugador 01");}
-        }
-
-    }
