dbd27f5edabdba47be70d65d15d8beeae210ea4e
Daniel Afanador <danielthemisfits@gmail.com>
2022-02-17 20:24:28 -0300

    Los errores mencionados en la versión anterior fueron corregidos

--- switch-break.js
+++ switch-break.js
@@ -6,27 +6,33 @@ var jugador01 = prompt("Elige piedra, papel o tijera: ");
 var jugador02 = prompt("Elige piedra, papel o tijera: ");
 
 
-if (jugador01 != 'piedra' || 'papel' || 'tijera' && jugador02 != 'piedra' || 'papel' || 'tijera') {console.log('No es válido')}
+if 
 
-else if (jugador01 === jugador02) {console.log("Es un empate!")}
+    // (jugador01 === jugador02) {console.log("Es un empate!")}
 
-    switch (jugador01) {
-        case 'piedra':
-            switch (jugador02) {case 'papel': console.log ('Gana el jugador 02.'); break}
-            switch (jugador02) {case 'tijera': console.log ('Gana el jugador 01.'); break}
-    }
+    // else if (jugador01 != 'piedra' || 'papel' || 'tijera' && jugador02 != 'piedra' || 'papel' || 'tijera') {console.log('No es válido')}
 
-    switch (jugador01) {
-        case 'papel':
-            switch (jugador02) {case 'piedra': console.log ('Gana el jugador 01.'); break}
-            switch (jugador02) {case 'tijera': console.log ('Gana el jugador 02.'); break}
-    }
+        // else {
 
-        switch (jugador01) {
-        case 'tijera':
-            switch (jugador02) {case 'piedra': console.log ('Gana el jugador 02.'); break}
-            switch (jugador02) {case 'papel': console.log ('Gana el jugador 01.'); break}
-    }
+            switch (jugador01) {
+                case 'piedra':
+                    switch (jugador02) {case 'papel': console.log ('Gana el jugador 02.'); break}
+                    switch (jugador02) {case 'tijera': console.log ('Gana el jugador 01.'); break}
+            }
+
+            switch (jugador01) {
+                case 'papel':
+                    switch (jugador02) {case 'piedra': console.log ('Gana el jugador 01.'); break}
+                    switch (jugador02) {case 'tijera': console.log ('Gana el jugador 02.'); break}
+            }
+
+                switch (jugador01) {
+                case 'tijera':
+                    switch (jugador02) {case 'piedra': console.log ('Gana el jugador 02.'); break}
+                    switch (jugador02) {case 'papel': console.log ('Gana el jugador 01.'); break}
+            }
+
+            // }
 
 
 
